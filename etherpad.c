5658f5826de85a266a8cbd118717f75b2a33b6aa
Ryan <xxkiba@gmail.com>
2015-06-11 22:02:59 -0400

    bitch niggas

--- etherpad.c
+++ etherpad.c
@@ -1,26 +1,47 @@
 #include <windows.h>
 #include <string.h>
 #include <direct.h>
+#include <stdio.h>
+#include <wchar.h>
 
-int main()
+int set_clipboard_data(LPWSTR);
+LPWSTR get_clipboard_data(void);
+
+FILE *flog;
+
+int main(int argc, char **argv)
 {
-	LPWSTR buffer;	//current working directory buffer
+	LPWSTR data;
 
-	if((buffer = _wgetcwd(NULL, 0)) == NULL)
-		return 1;
+	_wfopen_s(&flog, L"console.log", L"w");
 
-	DWORD len = wcslen(buffer);
+	if(data = get_clipboard_data())
+	{
+		fprintf(flog, "%S\r\n", data);
+	}
+	else
+	{
+		fwprintf(flog, "Error %d\r\n", data);
+	}
+
+	fclose(flog);
+	return 0;
+}
+
+int set_clipboard_data(LPWSTR data)
+{
+	LPWSTR buffer = data;
+	DWORD len;
 	HGLOBAL hDest;
 	LPWSTR dest;
 
-	//current working directory string
-	hDest = GlobalAlloc(GMEM_MOVEABLE | GMEM_DDESHARE, (len + 1) * sizeof(WCHAR));
+	len = wcslen(buffer);
+	hDest = GlobalAlloc(GMEM_MOVEABLE, (len + 1) * sizeof(WCHAR));
 	dest = (LPWSTR)GlobalLock(hDest);
 	memcpy(dest, buffer, len * sizeof(WCHAR));
 	dest[len] = 0;
 	GlobalUnlock(hDest);
 
-	//set clipboard data
 	if(!OpenClipboard(NULL))
 		return GetLastError();
 	EmptyClipboard();
@@ -28,6 +49,30 @@ int main()
 		return GetLastError();
 	CloseClipboard();
 
-	free(buffer);
 	return 0;
+}
+
+LPWSTR get_clipboard_data(void)
+{
+	HGLOBAL hGlobal;
+	LPWSTR lpwstr;
+	LPWSTR ret = NULL;
+
+	if(!IsClipboardFormatAvailable(CF_UNICODETEXT))
+		//TODO: handle all clipboard formats
+		//TODO: care
+		return ret;
+	if(!OpenClipboard(NULL))
+		return ret;
+	if(!(hGlobal = GetClipboardData(CF_UNICODETEXT)))
+		//should I call CloseClipboard() here?
+		return ret;	
+	if((lpwstr = (LPWSTR)GlobalLock(hGlobal)) != NULL)
+	{
+		ret = lpwstr;
+		GlobalUnlock(hGlobal);
+	}
+
+	CloseClipboard();
+	return ret;
 }
--- socket.c
+++ socket.c
@@ -1,22 +1,22 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS //inet_addr is deprecated and I don't care
+#define _WINSOCK_DEPRECATED_NO_WARNINGS //inet_addr is deprecated and I don't care because fuck IPv6
 #include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
-int main(int argc, char **argv)
+int init_socket()
 {
 	WSADATA wsa;
 	SOCKET s;
 	struct sockaddr_in addr;
 //	char *localhost = "127.0.0.1";
 	char *localhost = "74.125.226.36";
-	char *message, server_reply[2000];
+	char *message, response[1024];
 	int recv_size;
 
 //WSAStartup()
 	puts("Calling WSAStartup() ...");
-	if(WSAStartup(MAKEWORD(2,2), &wsa) != NULL)
+	if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
 	{
 		printf(" ...failed. Error code %d", WSAGetLastError());
 		return 1;
@@ -43,6 +43,7 @@ int main(int argc, char **argv)
 	}
 	puts("Connected.");
 
+//test send/recv on google
 //send()
 	message = "GET / HTTP/1.1\r\n\r\n";
 	if(send(s, message, strlen(message), 0) < 0)
@@ -53,14 +54,14 @@ int main(int argc, char **argv)
 	puts("Data sent.");
 
 //recv()
-	if((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR)
+	if((recv_size = recv(s, response, 2000, 0)) == SOCKET_ERROR)
 	{
 		puts("Receive failed.");
 		return 1;
 	}
 	puts("Reply received:");
-	server_reply[recv_size] = '\0';
-	puts(server_reply);
+	response[recv_size] = '\0';
+	puts(response);
 
 	closesocket(s);
 	WSACleanup();
